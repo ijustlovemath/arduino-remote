48c4d548dea9e56225cc5879a1a60fc444ab5dfb
Jeremy DeJournett <jcdejournett@gmail.com>
2022-03-22 10:12:09 -0400

    Completed basic API, now to deploy

--- py/arduino_remote_app/api/urls.py
+++ py/arduino_remote_app/api/urls.py
@@ -2,5 +2,7 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path("volume/up", views.volume_up)
+    path("volume/up", views.volume_up),
+    path("volume/down", views.volume_down),
+    path("power/toggle", views.power_toggle),
 ]
--- py/arduino_remote_app/api/views.py
+++ py/arduino_remote_app/api/views.py
@@ -4,8 +4,15 @@ from rest_framework.decorators import api_view
 from .remote import query
 
 # Used a lot from here: https://dev.to/nobleobioma/create-a-simple-rest-api-with-django-253p
-# Create your views here.
 @api_view(["GET"])
 def volume_up(request):
     content = query("volume up")
     return JsonResponse(content)
+
+@api_view(["GET"])
+def volume_down(request):
+    return JsonResponse(query("volume down"))
+
+@api_view(["GET"])
+def power_toggle(request):
+    return JsonResponse(query("power"))
